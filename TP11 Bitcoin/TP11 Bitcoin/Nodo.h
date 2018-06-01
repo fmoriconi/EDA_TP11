6a2e64a15d555053dfd1ad3d416f5dbac17460ee
fmoriconi <31166325+fmoriconi@users.noreply.github.com>
2018-06-01 11:03:26 -0300

    SASAAAGEYO, SASAAAGEYO

---
+++ TP11 Bitcoin/TP11 Bitcoin/Nodo.h
@@ -0,0 +1,26 @@
+#pragma once
+class Nodo
+{
+public:
+	Nodo();
+	~Nodo();
+
+
+	receiveTX(TX, PubKey);
+	receiveBlock(Block,) PubKey;
+	inputQueue;
+	outputQueue;
+	checkedTX(bool, Tx);
+	checkedBlock( , );
+
+
+	//Si el nodo es minero
+	mine(); //Minar = Probar un solo valor por nodo minero en cada loop.
+	createBlock();
+
+private:
+
+
+
+};
+
---
+++ TP11 Bitcoin/TP11 Bitcoin/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+//#include "cryptopp"
+int main(int argc, char * argv[]) {
+
+	createNodes();
+
+	while (checkConnectivity()) //BFS o DFS...
+		makeConnections();
+
+
+
+
+	for (Node N in Graph) {
+
+		if (N.hasTX2Send())
+			N.sendTX();
+
+	} //Primero se envian todas las transacciones, apilandose en una cola como en networking, y luego se procesan.
+
+	for (Node N in Graph) {
+		if (N.TXArrived)
+			N.VerifyTxQueue();
+	}
+
+	for (N in graph) {
+		if (N.isMiner())
+			N.OneMine(); //ONE MIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIINE
+	}
+
+
+	createTX(N1, N2, 7); //Mando 7 EDACoins del nodo 1 al nodo 2
+
+	//----------------------------//
+	// ALLEGRO	- Input y eventos //
+	//----------------------------//
+
+}
+
+//Si nos da una embolia, revisar bitcoinbook en gitHub. Sirve para entender los conceptos.
+
+//Para verificar la transaccion tiene que estar en el UTXO y si no esta en la UTXO, ver si esta en PENDING.
+//Si esta en la UTXO, buscamos la transaccion en el blockchain y validamos toda la transaccion.
+//Si esta en PENDING, una transaccion de pending referencia a algo en el blockchain de UTXO. Esto se cumples siempre.
+//Verificar coherencia de la entrada y salida de transaccion. Si entran dos, salen dos.
+//La clave publica esta en la output transaction.
+// {prevUTXO, Sign}, {pubKey, $} (?)
+//
